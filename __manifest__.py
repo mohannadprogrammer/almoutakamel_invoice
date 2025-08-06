72adcb0e3144adceccf2e7ca9eb873aaf4e4dd5a
mohannadprogrammer <waheedmohannad55578@gmail.com>
2025-08-06 03:14:49 +0200

    [add] add new external layout in odoo configration

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Almoutakamel_invoice ',
+    'version': '1.0',
+    'description': 'This module for  Almoutakamel invoice requirements',
+    'summary': 'Almoutakamel Required Invoice ',
+    'author': 'Mohannad Waheed',
+    'sequence': 1,
+    'license': 'LGPL-3',
+    'category': 'accounting',
+    'depends': [
+        'base',
+        'account',
+        'web'
+    ],
+    'data': [
+        'templates/motakamel_extenal_layout.xml'
+    ],
+    'demo': [
+        'data/report_layout.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'assets': {
+        
+    }
+}
